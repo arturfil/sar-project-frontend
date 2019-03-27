0cb3aee033be55bf6ad4764d84629ef8024695a7
Arturo Filio Villa <arturfil@hotmail.com>
2019-03-27 03:46:46 -0400

    first commit

---
+++ src/components/volcanoes/Volcanoes.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { Card } from 'react-bootstrap'
+
+class Volcanoes extends Component {
+   constructor() {
+    super();
+    this.state = {
+      volcanoes: []
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('/api/volcanoes')
+      .then(res => res.json())
+      .then(volcanoes => this.setState({volcanoes}, () => console.log('working', volcanoes)));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <ul>
+            <div className="row">
+              {this.state.volcanoes.map(volcano => 
+              <div className="col-4">
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant='top' src={volcano.img} />
+                  <Card.Body>
+                    <Card.Title>{volcano.name}</Card.Title>
+                    <Card.Text>
+                      Location: {volcano.location},
+                    </Card.Text>
+                    <Card.Text>
+                      Elvation: {volcano.height}'
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+              )}
+            </div>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Volcanoes;
